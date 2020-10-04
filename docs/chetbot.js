71dfc0e6cca3fe363c54aaff71684c3757285d01
EMC-prog <65492923+EMC-prog@users.noreply.github.com>
2020-10-04 17:40:22 +0200

    Delete chetbot.js

--- docs/chetbot.js
+++
@@ -1,8 +0,0 @@
-function saveUn() {
-    var f = document.getElementById('f').value;
-    if (f == "hello" || f == "good morning" || f == "good afternoon" || f == "good night")
-        alert("Hello, human.");
-    if (f == "what day is today")
-        alert(new Date());
-    return false;
-}
